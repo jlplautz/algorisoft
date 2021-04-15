a60bff72f2aa46075e90af95740b1ff2f786bf83
61246880 <jorge.plautz@gmail.com>
2021-04-15 15:27:06 -0300

    Ambiente virtual criado e outras implementaçoes
    close #1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cursoDjango3
 Projeto baseado no site Algorisoft - Python com Django
+
+## Como verificar a estrutura dos diretorio no projeto
+  ### abrir o python console
+  - >>> import os
+  - >>> BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  - >>> BASE_DIR
+      - '/home/plautz/PycharmProjects' 
+        
+  - >>> os.path.join(BASE_DIR, 'templates')
+
+      - '/home/plautz/PycharmProjects/templates'
+
--- src/settings.py
+++ src/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-!98ojbt9a8b!mc3r-#de*-o_m@i)&wa8am_zd5bto^6it35^^e'
+SECRET_KEY = 'django-insecure-!98ojbt9a8b!mc3r-#de*-o_m@i)&wa8am_zd5bto^6it35^^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

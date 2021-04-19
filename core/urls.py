0e53100065ac8c67e6722d2988bf2532c07e51e6
61246880 <jorge.plautz@gmail.com>
2021-04-19 12:40:02 -0300

    Criado pagina para listar categoria com CBV
    close #10

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from core.views.category.views import category_list
+from core.views.category.views import CategoryListView
 
 app_name = 'core'
 
 urlpatterns = [
-    path('category/list', category_list, name='category_list'),
-
+    path('category/list', CategoryListView.as_view(), name='category_list')
 ]

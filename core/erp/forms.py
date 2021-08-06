f41aae8607e63a2365cf45f229f1a2091079ef5c
61246880 <jorge.plautz@gmail.com>
2021-08-06 19:27:49 -0300

    Implementado registros de clientes com Modal
    close #50

--- core/erp/forms.py
+++ core/erp/forms.py
@@ -1,5 +1,7 @@
-from django.forms import ModelForm, Form, Textarea, TextInput, ModelChoiceField, Select
-from core.erp.models import Category, Product
+from datetime import datetime
+
+from django.forms import ModelForm, Form, Textarea, TextInput, ModelChoiceField, Select, DateInput
+from core.erp.models import Category, Product, Client
 
 
 class CategoryForm(ModelForm):
@@ -93,3 +95,53 @@ class TestForm(Form):
     products = ModelChoiceField(queryset=Product.objects.none(), widget=Select(attrs={
         'class': 'form-control'
     }))
+
+
+class ClientForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['names'].widget.attrs['autofocus'] = True
+
+    class Meta:
+        model = Client
+        fields = '__all__'
+        widgets = {
+            'names': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese sus nombres',
+                }
+            ),
+            'surnames': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese sus apellidos',
+                }
+            ),
+            'dni': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su dni',
+                }
+            ),
+            'date_birthday': DateInput(format='%Y-%m-%d', attrs={
+                    'value': datetime.now().strftime('%Y-%m-%d'),
+                }
+            ),
+            'address': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su dirección',
+                }
+            ),
+            'gender': Select()
+        }
+        exclude = ['user_updated', 'user_creation']
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                form.save()
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
--- core/erp/models.py
+++ core/erp/models.py
@@ -61,12 +61,18 @@ class Client(models.Model):
     names = models.CharField(max_length=150, verbose_name='Nome')
     surnames = models.CharField(max_length=150, verbose_name='SobreNome')
     dni = models.CharField(max_length=10, unique=True, verbose_name='Dni')
-    birthday = models.DateField(default=datetime.now, verbose_name='Data de nascimento')
+    date_birthday = models.DateField(default=datetime.now, verbose_name='Data de nascimento')
     address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Endereço')
-    sexo = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
+    gender = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
 
     def __str__(self):
         return self.names
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        item['gender'] = self.get_gender_display()
+        item['date_birthday'] = self.date_birthday.strftime('%Y-%m-%d')
+        return item
 
     class Meta:
         verbose_name = 'Cliente'
